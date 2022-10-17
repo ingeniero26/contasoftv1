54e130c97203eccf72162f5a20be4112f3339b1c
ingeniero26 <ingjerson2014@gmail.com>
2022-10-17 16:14:42 -0500

    chore(ARL)Cambio al insertar ARL(CONTROLADOR)

--- controlador/configuracion/control_arl_registro.php
+++ controlador/configuracion/control_arl_registro.php
@@ -8,8 +8,9 @@ $ciudad = htmlspecialchars($_POST['ciudad'],ENT_QUOTES,'UTF-8');
 $direccion = htmlspecialchars($_POST['direccion'],ENT_QUOTES,'UTF-8');
 $telefono = htmlspecialchars($_POST['telefono'],ENT_QUOTES,'UTF-8');
 $correo = htmlspecialchars($_POST['correo'],ENT_QUOTES,'UTF-8');
+$idempresa = htmlspecialchars($_POST['idempresa'],ENT_QUOTES,'UTF-8');
 
-$consulta =$MCT->Registrar_Arl($nit,$nombre,$ciudad,$direccion,$telefono,$correo);
+$consulta =$MCT->Registrar_Arl($nit,$nombre,$ciudad,$direccion,$telefono,$correo,$idempresa);
 echo $consulta;
 
 
