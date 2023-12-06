db2b364513da0c0e2a8e5bf26519ccf073b6cacd
ingeniero26 <ingjerson2014@gmail.com>
2023-12-06 07:38:07 -0500

    bug cadena de conexion reportes

--- conexion_global/r_conexion.php
+++ conexion_global/r_conexion.php
@@ -1,6 +1,6 @@
 <?php 
 	
-	$conexion = new mysqli('localhost','root','','sistema_pos_v2');
+	$conexion = new mysqli('localhost','root','','sistema_pos_v2_desarrollo');
 	if(mysqli_connect_errno()) {
 		echo "conexion fallida",mysqli_connect_errno();
 		exit();
