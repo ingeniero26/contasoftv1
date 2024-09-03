2343f94855cc33c875fa5c247ae23fd7c13364e7
ingeniero26 <ingjerson2014@gmail.com>
2024-09-03 08:23:19 -0500

    ajustes contsafot

--- controller/cuentas_clientes/control_listar_combo_venta.php
+++ controller/cuentas_clientes/control_listar_combo_venta.php
@@ -1,5 +1,5 @@
 <?php 
-require '../../modelo/modelo_cuentas_clientes.php';
+require '../../model/modelo_cuentas_clientes.php';
 
 $MCC = new Modelo_Cuentas_Clientes();
 $consulta =$MCC->listar_combo_venta();
